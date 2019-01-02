91ff7c1b7b415fcbc61ada6b4dc8424f85472a71
Apollo Tang <tang.apollo@gmail.com>
2019-01-02 03:23:28 -0500

    add ref, add clearCache, add comment to my-async-fn.js

--- src/my-async-fn.js
+++ src/my-async-fn.js
@@ -1,4 +1,4 @@
-// import regeneratorRuntime from 'regenerator-runtime/runtime'; // works if not preset, this is non ideal
+// import regeneratorRuntime from 'regenerator-runtime/runtime'; // works if not preset, this is not the place to polyfill
 
 const myAsyncFunction = async function() {
   return 'myAsyncFunction'
